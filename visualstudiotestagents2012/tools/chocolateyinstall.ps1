df217afcf748edd4e5772efaaab2d81f6f043da4
Darwin Sanoy <DSanoy@workwave.com>
2015-12-18 16:55:55 +0000

    updates

--- visualstudiotestagents2012/tools/chocolateyinstall.ps1
+++ visualstudiotestagents2012/tools/chocolateyinstall.ps1
@@ -46,18 +46,23 @@ if ($packageParameters) {
 
 Write-Output "Logs for installers will be in $env:temp"
 
+$AvailableDriveLetter = @(65..90 | ForEach-Object {[char]$_ + ":"}) | Where-Object {@(get-wmiobject win32_logicaldisk | select -expand deviceid) -notcontains $_} | select-object -last 1
+
 try {
     Get-ChocolateyWebFile "$packageName" "$env:temp\VS2012.4_Agents_ENU.iso" $url -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64
-    imdisk -a -f "$env:temp\VS2012.4_Agents_ENU.iso"  -m "q:"
+    imdisk -a -f "$env:temp\VS2012.4_Agents_ENU.iso"  -m "$AvailableDriveLetter"
     If (!$ControllerInsteadofTestAgent)
     {
-      Install-ChocolateyInstallPackage 'visualstudiotestagent' 'exe' $silentArgs 'q:\testagent\vstf_testagent.exe'
+      Install-ChocolateyInstallPackage 'visualstudiotestagent' 'exe' $silentArgs "$AvailableDriveLetter\testagent\vstf_testagent.exe"
     }
     Else
     {
-      Install-ChocolateyInstallPackage 'visualstudiotestagent' 'exe' "/silent /log $env:temp\vstestcontrollerinstall.log" 'q:\TestController\vstf_testcontroller.exe'
+      Install-ChocolateyInstallPackage 'visualstudiotestagent' 'exe' "/silent /log $env:temp\vstestcontrollerinstall.log" "$AvailableDriveLetter\TestController\vstf_testcontroller.exe"
     }
-    imdisk -d -m q:
+    start-sleep -seconds 5
+    Try {start-process imdisk -argumentlist "'"-d -m $AvailableDriveLetter" -ErrorAction SilentlyContinue}
+    Catch {#swallow dismount ISO errors
+    }
 }
 catch {
     throw $_.exception

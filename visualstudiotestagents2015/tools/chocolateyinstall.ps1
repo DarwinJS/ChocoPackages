5e31a3f5d8c982d9a797dffed9cdaaaa9035de24
Darwin Sanoy <DSanoy@workwave.com>
2016-01-02 15:30:54 +0000

    Various corrections.

--- visualstudiotestagents2015/tools/chocolateyinstall.ps1
+++ visualstudiotestagents2015/tools/chocolateyinstall.ps1
@@ -6,10 +6,11 @@ $fileType   = 'EXE'
 $url        = 'https://download.microsoft.com/download/8/A/F/8AFFDD5A-53D9-46EB-98D7-B61BBCAF0DE6/vstf_testagent.exe'
 $logPath    = "$env:temp\vstestagents2015install.log"
 $silentArgs = "/Full /NoRestart /Q /Log $logPath"
+$validExitCodes = @(0,3010,1641)
 
-$checksum      = $checksum64     ='EB982D5B9C23DB45824DEAD444F2DB08CC3C741F'
+$checksum      = $checksum64     ='69CC5B261F6D3A7BE28CCC5D46B9936747F1F87C'
 $checksumType  = $checksumType64 = 'sha1'
 
 Write-Output "Logs for installer is here: $logPath"
 
-Install-ChocolateyPackage -PackageName "$packageName" -FileType $fileType -SilentArgs $SilentArgs -url $url -checksum $checksum -checksumType $checksumType
+Install-ChocolateyPackage -PackageName "$packageName" -FileType $fileType -SilentArgs $SilentArgs -url $url -validExitCodes $validExitCodes -checksum $checksum -checksumType $checksumType

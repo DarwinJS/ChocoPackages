cdd67c479d3dc0f7fa3d1e1feed05b6d45bc9dc7
DarwinJS <DarwinJS@sanoys.com>
2016-01-02 11:52:59 -0500

    Added previewconfig

---
+++ previewconfig/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageName = 'previewconfig' # arbitrary name for the package, used in messages
+$url = 'http://www.winhelponline.com/utils/previewconfig.zip' # download url
+$validExitCodes = @(0)
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Write-Output "*************************************************************************************************************************************"
+Write-Output "*  INSTRUCTIONS: Type `"previewconfig.exe`" to edit file type associations.                                                         *"
+Write-Output "*       More Info: http://www.winhelponline.com/blog/previewconfig-tool-registers-file-types-for-the-preview-pane-in-windows-vista/ *"
+Write-Output "*************************************************************************************************************************************"
---
+++ previewconfig/tools/chocolateyUninstall.ps1
@@ -0,0 +1,2 @@
+
+Uninstall-ChocolateyZipPackage previewconfig previewconfig.zip

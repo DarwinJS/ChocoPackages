75b637ed5a8bdcebdd4375dbfb1f1d6ee2225531
DarwinJS <DarwinJS@sanoys.com>
2016-01-02 12:29:54 -0500

    Eliminated use of msiexec.exe

--- VirtualMachineConverter/tools/chocolateyUninstall.ps1
+++ VirtualMachineConverter/tools/chocolateyUninstall.ps1
@@ -2,13 +2,9 @@ $packageName = 'virtualmachineconverter'
 $installerType = 'MSI'
 $url = 'http://download.microsoft.com/download/9/1/E/91E9F42C-3F1F-4AD9-92B7-8DD65DA3B0C2/mvmc_setup.msi'
 $validExitCodes = @(0,3010)
+$silentArgs = "{332C1E78-1D2F-4A64-B718-68095DC6254B} /qn /l*v $env:temp\uninstallvirtualmachineconverter.log"
 
-Try
-  {
-  Start-Process "msiexec.exe"  -ArgumentList "/x {332C1E78-1D2F-4A64-B718-68095DC6254B} /qn /l*v $env:temp\uninstallvirtualmachineconverter.log" -Wait
-  }
-
-Catch
-  {
-  "The installation of $packageName failed, please see installer log files at `"$env:temp\uninstallvirtualmachineconverter.log`" for more details." | write-host -foregroundcolor yellow
-  }
+Uninstall-ChocolateyPackage -PackageName $packageName `
+                                -FileType $installerType `
+                                -SilentArgs "$silentArgs" `
+                                -ValidExitCodes $validExitCodes

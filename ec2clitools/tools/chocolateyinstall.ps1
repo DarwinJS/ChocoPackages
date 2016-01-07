40151cc7b06a3d4cb5afca48c33069fc34fc9ec4
Darwin Sanoy <DSanoy@workwave.com>
2016-01-07 14:55:11 +0000

    Final code for ec2clitools

--- ec2clitools/tools/chocolateyinstall.ps1
+++ ec2clitools/tools/chocolateyinstall.ps1
@@ -2,11 +2,16 @@
 $ErrorActionPreference = 'Stop'; # stop on all errors
 
 $packageName= 'ec2clitools'
+$packageVersion = '1.7.5.1'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'http://s3.amazonaws.com/ec2-downloads/ec2-api-tools.zip'
+$checksum = '51D6D7DFAFBFEDF74C743DAB6B9B58A4E48232DF'
+$checksumtype = 'sha1'
 
-Install-ChocolateyZipPackage $packageName $url $toolsDir
+Install-ChocolateyZipPackage $packageName $url $toolsDir -checksum $checksum -checksumType $checksumType
 
-Install-ChocolateyEnvironmentVariable -variableName "EC2_HOME" -variableValue "value" -variableType = 'Machine'
+Install-ChocolateyEnvironmentVariable -variableName 'EC2_HOME' -variableValue "C:\ProgramData\chocolatey\lib\ec2clitools\tools\ec2-api-tools-$packageVersion" -variableType 'Machine'
 
-Install-ChocolateyPath 'LOCATION_TO_ADD_TO_PATH' 'Machine'
+Install-ChocolateyPath "`%EC2_HOME`%\bin" 'Machine'
+
+Write-Warning "For completely automatic operation you must set the environment variables AWS_ACCESS_KEY and AWS_SECRET_KEY, for details see: http://docs.aws.amazon.com/AWSEC2/latest/CommandLineReference/set-up-ec2-cli-windows.html"
--- ec2clitools/tools/chocolateyuninstall.ps1
+++ ec2clitools/tools/chocolateyuninstall.ps1
@@ -2,6 +2,27 @@
 $ErrorActionPreference = 'Stop';
 
 $packageName = 'ec2clitools'
-$installerType = 'MSI' 
+$zipName = 'ec2-api-tools.zip'
 
-Uninstall-ChocolateyZipPackage
+Uninstall-ChocolateyZipPackage $packageName $zipName
+
+[Environment]::SetEnvironmentVariable('EC2_HOME',$null,'Machine')
+
+#Using .NET method prevents expansion (and loss) of environment variables (whether the target of the removal or not)
+#To avoid bad situations - does not use substring matching or regular expressions
+#Removes duplicates of the target removal path, Cleans up double ";", Handles ending "\"
+
+$PathToRemove = '%EC2_HOME%\bin'
+foreach ($path in [Environment]::GetEnvironmentVariable("PATH","Machine").split(';'))
+{
+  If ($Path)
+  {
+    If (($path -ine "$PathToRemove") -AND ($path -ine "$PathToRemove\"))
+    {
+      [string[]]$Newpath += "$path"
+    }
+  }
+}
+$AssembledNewPath = ($newpath -join(';')).trimend(';')
+
+[Environment]::SetEnvironmentVariable("PATH",$AssembledNewPath,"Machine")

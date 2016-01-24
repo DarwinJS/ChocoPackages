7259211233798441a14fd7671cfdddf277f18ed3
DarwinJS <DarwinJS@sanoys.com>
2016-01-24 08:46:00 -0500

    new packages and updates

--- systemexplorer/tools/chocolateyInstall.ps1
+++ systemexplorer/tools/chocolateyInstall.ps1
@@ -10,8 +10,16 @@ If (!(Test-Path $AppPathKey)) {New-Item "$AppPathKey" | Out-Null}
 Set-ItemProperty -Path $AppPathKey -Name "(Default)" -Value "$env:chocolateyinstall\lib\$packagename\tools\$exeName"
 Set-ItemProperty -Path $AppPathKey -Name "Path" -Value "$env:chocolateyinstall\lib\$packagename\tools\"
 
-
 Install-ChocolateyZipPackage "$packageName" "$toolsDir\SystemExplorerPortable_700.zip" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+
+#Create .ignore files so chocolatey does not shim the Exe
+$files = get-childitem $toolsDir -include *.exe -recurse
+foreach ($file in $files) {
+  New-Item "$file.ignore" -type file -force | Out-Null
+}
+
+Move-Item "$toolsDir\systemexplorer.exe.ignore" "$toolsDir\systemexplorer.exe.gui" -force
 
 Write-Output "******************************************************************************"
 Write-Output "*  INSTRUCTIONS: In the search prompt of your start menu type `"$exeName`"   *"

2547fcb322a33185b3301f44d3676617bcc7f33f
DarwinJS <DarwinJS@sanoys.com>
2016-03-20 08:48:53 -0400

    SpyStudio version upgrade

--- spystudio/tools/chocolateyInstall.ps1
+++ spystudio/tools/chocolateyInstall.ps1
@@ -1,17 +1,23 @@
-﻿$packageName = 'NektraSpyStudio' # arbitrary name for the package, used in messages
-$url = 'http://www.nektra.com/files/SpyStudio/SpyStudio-v2.zip' # download url
-$additionalurlfor64 = 'http://www.nektra.com/files/SpyStudio/SpyStudio-v2-x64.zip' # 64bit URL here or remove - if installer decides, then use $url
-$silentArgs = 'SILENT_ARGS_HERE' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
-$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
-#Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+$ErrorActionPreference = 'Stop';
+$packageName = 'SpyStudio'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-If ((!(Test-Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5")) -OR (!((get-itemproperty "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5").sp -ge 1)))
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = 'http://www.nektra.com/files/SpyStudio/SpyStudio-v2.zip'
+  url64         = 'http://www.nektra.com/files/SpyStudio/SpyStudio-v2-x64.zip'
+  validExitCodes= @(0, 3010, 1641)
+}
+
+#Find whether there is a qualifying version of .NET installed
+If (((Test-Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5") -AND (!((get-itemproperty "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5").sp -ge 1))) -OR (![bool]((gci 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\NET Framework Setup\NDP') | select-string -pattern @('v4','v4.0') -simplematch)))
   {
-  Throw "Spy Studio requires .NET 3.5.1 or later.  You must either: [a] manually install it after verifing it will not cause problems on this system, or [b] use procmon for your trace and analyze it using Spy Studio installed on another system that has .NET 3.5.1 or later."
+  Throw "Spy Studio requires .NET 3.5.1 or later.  You must either: [a] manually install a qualifying version of .NET after verifing it will not cause problems on this system, or [b] use procmon for your trace and analyze it using Spy Studio installed on another system that has .NET 3.5.1 or later.  FYI: You can install it using the package 'dotnet3.5'"
   }
 
-Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage @packageArgs
 
 Write-Output "***************************************************************************"
 Write-Output "*  INSTRUCTIONS: Type `"spystudio.exe`" to start SpyStudio.               *"

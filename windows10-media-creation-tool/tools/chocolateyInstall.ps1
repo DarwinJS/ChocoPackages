6b736712c11c97e51880a764aece03d80f605903
DarwinJS <DarwinJS@sanoys.com>
2016-03-20 10:09:50 -0400

    windows10-media-creation-tool - fixes

--- windows10-media-creation-tool/tools/chocolateyInstall.ps1
+++ windows10-media-creation-tool/tools/chocolateyInstall.ps1
@@ -2,12 +2,8 @@ $PackageName = 'windows10-media-creation-tool'
 $exeName = "MediaCreationTool.exe"
 $url = 'http://go.microsoft.com/fwlink/?LinkId=691209'
 $validExitCodes = @(0)
-$checksum      = '262088CF4B0F05D30DB0FE86757E306F0AAA4E9B'
-$checksumType  = 'sha1'
 
-Get-ChocolateyWebFile -packagename "$packageName" -url "$url" -filefullpath "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\$exeName" -checksum $checksum -checksumType $checksumType -GetOriginalFileName
-
-
+Get-ChocolateyWebFile -packagename "$packageName" -url "$url" -filefullpath "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\$exeName" -GetOriginalFileName
 
 $AppPathKey = "Registry::HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\$exeName"
 If (!(Test-Path $AppPathKey)) {New-Item "$AppPathKey" | Out-Null}

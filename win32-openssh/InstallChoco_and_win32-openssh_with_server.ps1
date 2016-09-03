0fb53071e8ab92a70b1ddcff54b25580866872e9
DarwinJS <DarwinJS@sanoys.com>
2016-09-03 05:38:55 -0400

    Updates to win32-openssh

--- win32-openssh/InstallChoco_and_win32-openssh_with_server.ps1
+++ win32-openssh/InstallChoco_and_win32-openssh_with_server.ps1
@@ -66,4 +66,4 @@ If (!(Test-Path env:ChocolateyInstall))
 
 Write-Output "Chocolatey is installed and enabled for use in this session..."
 
-choco install win32-openssh -params '"/SSHServerFeature /KeyBasedAuthenticationFeature"' -confirm
+choco install win32-openssh -params '"/SSHServerFeature"' -confirm

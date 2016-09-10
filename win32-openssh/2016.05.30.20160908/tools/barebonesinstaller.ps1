28c6579c86548a73043046a5ecc978a86c2e8d56
DarwinJS <DarwinJS@sanoys.com>
2016-09-09 21:58:22 -0400

    Final updates for Nano

--- win32-openssh/2016.05.30.20160908/tools/barebonesinstaller.ps1
+++ win32-openssh/2016.05.30.20160908/tools/barebonesinstaller.ps1
@@ -1,5 +1,8 @@
 <#
-This script allows the SSH install to proceed even when your system does not have:
+.SYNOPSIS
+Enables installing SSH even when your system does not have WOW64 or Chocolatey.
+.DESCRIPTION
+This script enables installing  SSH even when your system does not have:
   [1] Chocolatey installed
   [2] WOW64 installed
 
@@ -9,16 +12,22 @@ To use barebonesinstaller.ps1, expand the .nupkg that this file is contained in
 and then place the \tools folder on the target system.
 
 To push tools folder to Nano use 'Copy-Item -tosession $sessionvariable tools c:\tools -recurse'
-
+.PARAMETER SSHServerFeature
+Include SSH Server Feature.
+.PARAMETER DeleteServerKeysAfterInstalled
+Delete server private keys after they have been secured
+.PARAMETER Uninstall
+Uninstall (default is to install)
+.EXAMPLE
 .\barebonesinstaller.ps1 -SSHServerFeature
-
+.EXAMPLE
 .\barebonesinstaller.ps1 -SSHServerFeature -Uninstall
 #>
 
 Param (
-  [Parameter(HelpMessage="Including SSH Server Feature.")]
+  [Parameter(HelpMessage="Include SSH Server Feature.")]
   [switch]$SSHServerFeature,
-  [Parameter(HelpMessage="Deleting server private keys after they have been secured.")]
+  [Parameter(HelpMessage="Delete server private keys after they have been secured.")]
   [switch]$DeleteServerKeysAfterInstalled,
   [Parameter(HelpMessage="Uninstall instead of Install (install is the default).")]
   [switch]$Uninstall

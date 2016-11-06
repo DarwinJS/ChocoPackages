9e34545418cafa256cece72df3f082ebf7de3c95
DarwinJS <DarwinJS@sanoys.com>
2016-11-06 08:10:51 -0500

    whocrashed version update and add checksum

--- whocrashed/tools/chocolateyInstall.ps1
+++ whocrashed/tools/chocolateyInstall.ps1
@@ -1,10 +1,20 @@
-﻿$packageName = 'whocrashed'
-$installerType = 'EXE'
-$url = 'http://www.resplendence.com/download/whocrashedSetup.exe'
-$silentArgs = "/VERYSILENT /NORESTART /LOG=`"$env:temp\CHOCO-INSTALL-whocrashed.log`""
-$validExitCodes = @(0)
+﻿
+$ErrorActionPreference = 'Stop';
+$packageName = 'whocrashed'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  installerType = 'EXE'
+  url           = 'http://www.resplendence.com/download/whocrashedSetup.exe'
+  checksum      = '3A8ED4F8D3EC0E64050BB6C4375B4523377BD39B'
+  checksumtype  = 'sha1'
+  silentArgs    = "/VERYSILENT /NORESTART /LOG=`"$env:temp\CHOCO-INSTALL-whocrashed.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageargs
 
 Write-Output "********************************************************************"
 Write-Output "*  INSTRUCTIONS: Use the start menu to search for `"WhoCrashed`"   *"

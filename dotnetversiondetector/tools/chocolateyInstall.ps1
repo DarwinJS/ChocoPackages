4380d0ecf1a8e7717ac8a3c71613a78653aaf530
DarwinJS <DarwinJS@sanoys.com>
2017-08-14 13:59:12 -0400

    version 17.2.2 of dotnetversiondetector

--- dotnetversiondetector/tools/chocolateyInstall.ps1
+++ dotnetversiondetector/tools/chocolateyInstall.ps1
@@ -13,15 +13,15 @@ $packageArgs = @{
   #file         = "$toolsDir\netver.zip"
 
   softwareName  = 'dotnetversiondetector*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum      = '4DA421599E5237B0005D8076D5D6880D2D018FE838EE5128447F0D38223E1F51'
+  checksum      = 'B76345AE3B4AC1401E55149E5BE3CD81D0DAA8A2DD3E2F2318226A49E21C390A'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
-  checksum64    = '4DA421599E5237B0005D8076D5D6880D2D018FE838EE5128447F0D38223E1F51'
+  checksum64    = 'B76345AE3B4AC1401E55149E5BE3CD81D0DAA8A2DD3E2F2318226A49E21C390A'
   checksumType64= 'sha256' #default is checksumType
 }
 
 Install-ChocolateyZipPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
 
-Move-Item $toolsdir\dotnet.exe $toolsdir\dotnetversions.exe
+Move-Item $toolsdir\dotnet.exe $toolsdir\dotnetversions.exe -Force
 
 
 Write-Output ""

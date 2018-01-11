ccd2fad1dc3dd82a2ed441176cd1e3fd63095f8d
Darwin <darwinjsgithub.services@sanoys.com>
2018-01-11 06:54:48 -0500

    powershell-core 6.0.0 GA

--- powershell-core/tools/chocolateyinstall.ps1
+++ powershell-core/tools/chocolateyinstall.ps1
@@ -3,11 +3,8 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'powershell-core'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$VersionMaj = '6.0.0'
-$versionMinor = '2'
-$Version = "$VersionMaj.$versionMinor"
-$PFSubfolder = "$VersionMaj-rc$versionMinor"
-$InstallFolder = "$env:ProgramFiles\PowerShell\$PFSubfolder"
+$Version = "6.0.0"
+$InstallFolder = "$env:ProgramFiles\PowerShell\$Version"
 
 If (Test-Path "$InstallFolder\powershell.exe")
 {
@@ -19,14 +16,14 @@ $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'MSI'
-  url           = 'https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-rc.2/PowerShell-6.0.0-rc.2-win-x86.msi'
-  url64bit      = 'https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-rc.2/PowerShell-6.0.0-rc.2-win-x64.msi'
+  url           = 'https://github.com/PowerShell/PowerShell/releases/download/v6.0.0/PowerShell-6.0.0-win-x86.msi'
+  url64bit      = 'https://github.com/PowerShell/PowerShell/releases/download/v6.0.0/PowerShell-6.0.0-win-x64.msi'
 
   softwareName  = "PowerShell-6.0.0*"
 
-  checksum      = 'E8A834D0785E0032E16E56351EF72CD1CDC4C3D9B2CA4DCAF86B070B72560A12'
+  checksum      = '494DF01BFF5A007F98761A5088E6E4AA6754808DEE0CBF096FFF171D1233E8FC'
   checksumType  = 'sha256'
-  checksum64    = 'DACE51D011EC5C0F9AC2B0E773799B2E8E5FDEA85375A569E98CA7C9C4A44D96'
+  checksum64    = 'A1155D0F9D697B3EBF51C03D328886F9000709C1C4688DA42FF9C234AF02A63F'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0

01731c92c6f476121b8244b232712eec93fa3da9
Darwin <darwinjsgithub.services@sanoys.com>
2019-03-07 06:47:53 -0500

    new rc

--- pwshpackages/powershell-core/tools/chocolateyinstall.ps1
+++ pwshpackages/powershell-core/tools/chocolateyinstall.ps1
@@ -3,8 +3,16 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'powershell-core'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$Version = "6.1.3"
-$InstallFolder = "$env:ProgramFiles\PowerShell\$Version"
+$Version = "6.2.0-rc.1"
+Try {
+  [Version]$Version
+  $InstallFolder = "$env:ProgramFiles\PowerShell\$($version.split('.')[0])"
+}
+Catch {
+  Write-output "Note: This is a prelease package"
+  $PreleasePackage = $true
+  $InstallFolder = "$env:ProgramFiles\PowerShell\$($version.split('.')[0])-preview"
+}
 
 If (Test-Path "$InstallFolder\pwsh.exe")
 {
@@ -21,9 +29,9 @@ $packageArgs = @{
 
   softwareName  = "PowerShell-6.0.*"
 
-  checksum      = 'BFDB11D5863CAC56C31AB05D07CDB66882E9EC5A93B73FB6F1860BD9FE34CC89'
+  checksum      = 'DEA825F48A666966B12B08BA224E8EF0BA6BE651DBD1B961193D4B1ECCA3F7F2'
   checksumType  = 'sha256'
-  checksum64    = '8C02B3E5375095C11135C74779F8F4A57A4E041A76C031D023673C6363814387'
+  checksum64    = '0C0F5D68D3C69D36BE4151C3C98BF44F3C6EA33769E19295BE136A785EC10054'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
@@ -42,11 +50,19 @@ Write-Warning "If you started this package under PowerShell core, replacing an i
 Install-ChocolateyPackage @packageArgs
 
 Write-Output "************************************************************************************"
-Write-Output "*  INSTRUCTIONS: Your system default PowerShell version has not been changed."
-Write-Output "*   PowerShell Core $version, was installed to: `"$installfolder`""
+Write-Output "*  INSTRUCTIONS: Your system default WINDOWS PowerShell version has not been changed."
+Write-Output "*   PowerShell CORE $version, was installed to: `"$installfolder`""
+If ($PreleasePackage) {
+Write-Output "*   To start PowerShell Core PRERELEASE $version, at a prompt execute:"
+Write-Output "*      `"$installfolder\pwsh.exe`""
+Write-Output "*   IMPORTANT: Prereleases are not put on your path, nor made the default version of CORE."
+}
+else {
 Write-Output "*   To start PowerShell Core $version, at a prompt or the start menu execute:"
 Write-Output "*      `"pwsh.exe`""
 Write-Output "*   Or start it from the desktop or start menu shortcut installed by this package."
+Write-Output "*   This is your new default version of PowerShell CORE (pwsh.exe)."
+}
 Write-Output "************************************************************************************"
 
 Write-Output "**************************************************************************************"

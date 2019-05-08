90bfc4fea431a1aff399465fd385659cbaf23a78
Darwin <darwinjsgithub.services@sanoys.com>
2019-05-08 06:38:41 -0400

    add draw.io

--- drawio/tools/chocolateyinstall.ps1
+++ drawio/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://github.com/jgraph/drawio-desktop/releases/download/v9.3.1/draw.io-setup-signed-9.3.1.exe'
 
@@ -6,16 +6,11 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  file          = $toolsDir/draw.io-setup-signed-9.3.1.exe
-  softwareName  = 'drawio*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  file          = "$toolsDir/draw.io-setup-signed-9.3.1.exe"
+  softwareName  = 'drawio*'
   checksum      = '4006D22608E2E8DADC7ED196B3F22EF14D301722B73C1D69FB26EF17858379FD'
-  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
-  checksum64    = '4006D22608E2E8DADC7ED196B3F22EF14D301722B73C1D69FB26EF17858379FD'
-  checksumType64= 'sha256' #default is checksumType
-
-  # MSI
-  silentArgs   = '/S'           # NSIS
-  #validExitCodes= @(0) #please insert other valid exit codes here
+  checksumType  = 'sha256'
+  silentArgs   = '/S'
 }
 
-Install-ChocolateyPackage @packageArgs # https://chocolatey.org/docs/helpers-install-chocolatey-package
+Install-ChocolateyPackage @packageArgs
